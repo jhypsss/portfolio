e774afbefd271a8e15fcfb57cc2492b7fe56d45e
Trixx <jhaypeetorres22@gmail.com>
2025-10-16 17:59:27 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
-    <title>Portfolio</title>
+    @section('title', config('app.name'))
+    <title>@yield('title')</title>      
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
     
